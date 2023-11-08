07c1c249e18797852b0a42d522709b6a8ee49193
PythonToGo <ty.kim@tum.de>
2023-11-08 10:19:17 +0100

    add ex.2

--- Intro2C++/exercise2/ex2.1_dataType.cpp
+++ Intro2C++/exercise2/ex2.1_dataType.cpp
@@ -8,3 +8,24 @@
 // -	Boolean value
 
 // Assign a value to the variables and then display them on the screen.
+
+#include <iostream>
+using namespace std;;
+
+int main(){
+    int number = 2;
+    float floatingNumber = 3.5;
+    string StringSentence = "Hello World";
+    char AlphabetA = 'A';
+    bool boolean = true;
+
+    cout << number << endl;
+    cout << floatingNumber << endl;
+    cout << StringSentence << endl;
+    cout << AlphabetA << endl;
+    cout << boolean << endl;
+
+    
+    return 0;
+
+};
